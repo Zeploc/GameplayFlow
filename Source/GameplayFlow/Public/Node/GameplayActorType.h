5bcda9f3d13c2e8af465acf8c83afddd3cc9a422
Alex Coultas <alexcoultasis@gmail.com>
2025-08-15 08:19:03 +1200

    DevelopmentOnly meta tag

--- Source/GameplayFlow/Public/Node/GameplayActorType.h
+++ Source/GameplayFlow/Public/Node/GameplayActorType.h
@@ -23,9 +23,9 @@ public:
 	virtual AActor* GetActor() const;
 	virtual void CleanUp();
 
-	UFUNCTION(BlueprintPure, DisplayName="Get Node Display", Category="Gameplay Actor Type")
+	UFUNCTION(BlueprintPure, DisplayName="Get Node Display", Category="Gameplay Actor Type", meta=(DevelopmentOnly))
 	FString K2_GetNodeDisplay() const;
-	UFUNCTION(BlueprintPure, DisplayName="Get Debug Display", Category="Gameplay Actor Type")
+	UFUNCTION(BlueprintPure, DisplayName="Get Debug Display", Category="Gameplay Actor Type", meta=(DevelopmentOnly))
 	FString K2_GetDebugDisplay() const;
 	
 #if WITH_EDITOR
